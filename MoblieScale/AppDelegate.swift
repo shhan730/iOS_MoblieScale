e8206e2b74f141e82abb802827d1cba24d2ffc11
SeungHun Han <shhan730@gmail.com>
2019-12-21 15:06:09 +0900

    Set View Controller

--- MoblieScale/AppDelegate.swift
+++ MoblieScale/AppDelegate.swift
@@ -8,30 +8,50 @@
 
 import UIKit
 
+enum Shortcut: String {
+  case openBlue = "OpenBlue"
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+    [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    print("didFinishLaunchingWithOptions called")
+    var isLaunchedFromQuickAction = false
+    
+    if let shortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+      isLaunchedFromQuickAction = true
+      let _ = handleQuickAction(shortcutItem)
+    } else {
+      self.window?.backgroundColor = UIColor.white
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    return !isLaunchedFromQuickAction
+  }
+  
+  func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+  
+  func handleQuickAction(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+    var quickActionHandled = false
+    let type = shortcutItem.type.components(separatedBy: ".").last!
+    
+    if let shortcutType = Shortcut.init(rawValue: type) {
+      switch shortcutType {
+      case .openBlue:
+        self.window?.backgroundColor = UIColor(red: 151.0/255.0, green: 187.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        quickActionHandled = true
+      }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
+    return quickActionHandled
+  }
 }
+
 
