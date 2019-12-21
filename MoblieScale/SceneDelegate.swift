fbafbf33bb3cdfac5022f154ce35017b9fed6880
SeungHun Han <shhan730@gmail.com>
2019-12-21 16:36:32 +0900

    Change Asset Files

--- MoblieScale/SceneDelegate.swift
+++ MoblieScale/SceneDelegate.swift
@@ -32,6 +32,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
